ee6d1511f30cebf2fa7e18e6b12a7754b5ad5414
michal <michal@capsicum.(none)>
2012-11-06 13:06:53 +0100

    fix synchro script

--- bin/synchronize.sh
+++ bin/synchronize.sh
@@ -20,16 +20,18 @@ echo -n $$ > $PIDFILE
 
 USR=michal
 SRV=192.168.0.21
-SRC=Pictures/
+SRC=Pictures/*
 DST=/mnt/HD/HD_a2/Pictures/
 DOFILE=$BIN_DIR/synchronize.done
 
-ping -q -i 60 -c 180 $SRV >/dev/null
-if [ $? -eq 0 ]
+while true; do ping -q -c1 $SRV >/dev/null && break; sleep 60; done
+echo "[$(date)] synchronizing..."
+rsync -av --delete $@ $USR@$SRV:$SRC $DST | egrep -e "\.[jJ][pP][gG]$" | egrep -ve "^deleting " >> $DOFILE
+ret=$?
+if [ $ret -gt 0 ]
 then
-  echo "[$(date)] synchronizing..."
-  rsync -av --delete $@ $USR@$SRV:$SRC $DST | egrep -e "\.[jJ][pP][gG]$" | egrep -ve "^deleting " >> $DOFILE
-  echo "[$(date)] synchronized"
+  echo "[$(date)] error $ret"
 fi
+echo "[$(date)] synchronized"
 
 rm $PIDFILE
